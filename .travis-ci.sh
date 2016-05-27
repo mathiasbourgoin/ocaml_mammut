b2b30ff867d756dda50f533eef3b108f47b26197
mathiasbourgoin <mathiasbourgoin@users.noreply.github.com>
2016-05-27 09:12:33 +0200

    Update .travis-ci.sh

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -13,8 +13,11 @@ case "$OCAML_VERSION,$OPAM_VERSION" in
 esac
 
 echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam libffi-dev
+
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam libffi-dev g++-4.8
+export CXX="g++-4.8"
 
 export OPAMYES=1
 export OPAMVERBOSE=1
