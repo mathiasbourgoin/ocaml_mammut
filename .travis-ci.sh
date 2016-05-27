1a5076cd2db64594131bf0f72f8e1f7111491972
mathiasbourgoin@ <mathias.bourgoin@gmail.com>
2016-05-27 09:53:03 +0200

    travis : add compilations flags for mammut

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -36,4 +36,4 @@ esac
 opam install ctypes ctypes-foreign
 #opam install ${OPAM_DEPENDS}
 
-git clone https://github.com/DanieleDeSensi/mammut.git && cd mammut/mammut && make && sudo make install
+git clone https://github.com/DanieleDeSensi/mammut.git && cd mammut/mammut && CXXFLAGS="-O3 -finlinefunctions --std=c++11" make && sudo make install
