2ddfd89d8c25906ac143c80f6c4520df2655f0ce
mathiasbourgoin@ <mathias.bourgoin@gmail.com>
2016-05-27 11:19:24 +0200

    travis : alias g++

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -18,9 +18,7 @@ sudo apt-get update -qq
 
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam libffi-dev g++-4.8
 export CXX="g++-4.8"
-
-sudo rm `which g++`
-sudo ln -s `which g++`-4.8 `which g++`
+alias g++='g++-4.8'
 
 export OPAMYES=1
 export OPAMVERBOSE=1
