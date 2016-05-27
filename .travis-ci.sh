ae697c7660b9bebbb817d32951da31911e9618e9
mathiasbourgoin@ <mathias.bourgoin@gmail.com>
2016-05-27 11:14:34 +0200

    travis : rm g++ link

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -19,6 +19,7 @@ sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam libffi-dev g++-4.8
 export CXX="g++-4.8"
 
+sudo rm `which g++`
 sudo ln -s `which g++`-4.8 `which g++`
 
 export OPAMYES=1
