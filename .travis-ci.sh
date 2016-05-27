631c6f4c6e7aee13ae923ca7a1dea4f19bff50e0
mathiasbourgoin@ <mathias.bourgoin@gmail.com>
2016-05-27 10:09:00 +0200

    travis : fix compilations flags for mammut

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -19,7 +19,7 @@ sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam libffi-dev g++-4.8
 export CXX="g++-4.8"
 
-sudo ln -s `wich g++`-4.8 `which g++`
+sudo ln -s `which g++`-4.8 `which g++`
 
 export OPAMYES=1
 export OPAMVERBOSE=1
