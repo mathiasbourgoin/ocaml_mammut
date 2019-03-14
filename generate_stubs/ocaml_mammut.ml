0802f56be17f3c7c91e1926beb909d7f9034d1eb
MBourgoin <mathias.bourgoin@gmail.com>
2019-03-14 10:43:01 +0100

    Complete binding (based on the partial C compatibily header from libmammut)

--- generate_stubs/ocaml_mammut.ml
+++ generate_stubs/ocaml_mammut.ml
@@ -4,10 +4,12 @@ open Cstubs
 module T = Generate_stubs.Bindings(Mammut_generated)
 
 
-let create_Mammut = T.create_Mammut
+
 
 module Mammut = struct
-  let getInstanceEnergy = T.mammut_getInstanceEnergy 
+  let create = T.create_Mammut
+  let destroy = T.destroy_Mammut
+  let getInstanceEnergy = T.mammut_getInstanceEnergy
 end
 
 module Energy = struct
@@ -17,6 +19,13 @@ end
 module Counter = struct
   let reset = T.counter_reset
   let getJoules = T.counter_get_Joules
+end
+
+module CounterCpus = struct
   let getJoulesCoresAll = T.counter_get_JoulesCoresAll
   let getJoulesDramAll = T.counter_get_JoulesDramAll
+  let getJoulesGraphicAll = T.counter_get_JoulesGraphicAll
+  let hasDram = T.counter_hasDram
+  let hasGraphic = T.counter_hasGraphic
+
 end
--- generate_stubs/test.ml
+++ generate_stubs/test.ml
@@ -7,20 +7,34 @@ let rec fibo = function
   | n -> fibo (n-1) + fibo (n-2)
 
 
+let print_joules = fun c ->
+  let joules = Counter.getJoules c
+  and joulesCoreAll = CounterCpus.getJoulesCoresAll c
+  and joulesDramAll =  CounterCpus.getJoulesDramAll c 
+  and joulesGraphicAll = CounterCpus.getJoulesGraphicAll c in
+  Printf.printf "  %g joules <--- getJoules \n" joules ;
+  Printf.printf "  %g joules <--- getJoulesCoreAll \n" joulesCoreAll ;
+  Printf.printf "  %g joules <--- getJoulesDramAll \n" joulesDramAll ;
+  Printf.printf "  %g joules <--- getJoulesGraphicAll \n" joulesGraphicAll 
+
 let _ = 
-  let m = create_Mammut () in
+  let m = Mammut.create () in
   let e = Mammut.getInstanceEnergy m in
   let c = Energy.getCounter e  in
-  Unix.sleep(2);
-  Printf.printf "%g joules consummed in the last 2 seconds \n%!" (Counter.getJoules c);
-  Counter.reset c;
-  Unix.sleep(4);
-  Printf.printf "%g joules consummed in the last 4 seconds \n%!"(Counter.getJoulesCoresAll c);
-  Counter.reset c;
+  Unix.sleep(2) ;
+  Printf.printf "consummed in the last 2 seconds \n%!" ;
+  print_joules c ;
+  Counter.reset c ;
+  Unix.sleep(4) ;
+  Printf.printf "consummed in the last 4 seconds \n%!" ;
+  print_joules c ;
+  Counter.reset c ;
   ignore(fibo 36);
-  Printf.printf "%g joules consummed computing fibo 36 \n%!" (Counter.getJoulesCoresAll c);
+  Printf.printf "consummed computing fibo 36 \n%!" ;
+  print_joules c ;
   Counter.reset c;
   ignore(fibo 48);
-  Printf.printf "%g joules consummed computing fibo 48 \n%!" (Counter.getJoulesDramAll c);
+  Printf.printf "consummed computing fibo 48 \n%!" ;
+  print_joules c;
   ()
 
