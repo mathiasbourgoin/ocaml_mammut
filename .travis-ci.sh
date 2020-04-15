e07c01f009523720ffef307d4c7fa340e887abe5
Mathias Bourgoin <mathias.bourgoin@nomadic-labs.com>
2020-04-15 15:45:24 +0200

    Update travis-ci

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,40 +1,40 @@
-# Thanks to frenetic-lang/ocaml-topology
-case "$OCAML_VERSION,$OPAM_VERSION" in
-3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
-3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
-4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
-4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
-4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
-4.02.0,1.1.0) ppa=avsm/ocaml42+opam11 ;;
-4.02.0,1.2.0) ppa=avsm/ocaml42+opam12 ;;
-4.02.3,1.2.0) ppa=avsm/ocaml42+opam12 ;;
-*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
-esac
+# # Thanks to frenetic-lang/ocaml-topology
+# case "$OCAML_VERSION,$OPAM_VERSION" in
+# 3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
+# 3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
+# 4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
+# 4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
+# 4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
+# 4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+# 4.02.0,1.1.0) ppa=avsm/ocaml42+opam11 ;;
+# 4.02.0,1.2.0) ppa=avsm/ocaml42+opam12 ;;
+# 4.02.3,1.2.0) ppa=avsm/ocaml42+opam12 ;;
+# *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
+# esac
 
-echo "yes" | sudo add-apt-repository ppa:$ppa
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-get update -qq
+# echo "yes" | sudo add-apt-repository ppa:$ppa
+# sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+# sudo apt-get update -qq
 
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam libffi-dev g++-4.8
-export CXX="g++-4.8"
+# sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam libffi-dev g++-4.8
+# export CXX="g++-4.8"
 
 
-export OPAMYES=1
-export OPAMVERBOSE=1
-echo OCaml version
-ocaml -version
-echo OPAM versions
-opam --version
-opam --git-version
+# export OPAMYES=1
+# export OPAMVERBOSE=1
+# echo OCaml version
+# ocaml -version
+# echo OPAM versions
+# opam --version
+# opam --git-version
 
-opam init
-eval `opam config env`
-case $OCAML_VERSION in
-    4.02|4.02.3) opam install camlp4;;
-esac
+# opam init
+# eval `opam config env`
+# case $OCAML_VERSION in
+#     4.02|4.02.3) opam install camlp4;;
+# esac
 #opam install ocp-build ctypes
-opam install ctypes ctypes-foreign ocamlfind
+opam install camlp4 ctypes ctypes-foreign ocamlfind
 #opam install ${OPAM_DEPENDS}
 
 git clone https://github.com/mathiasbourgoin/mammut.git && cd mammut && sed -i 's/g++/g++-4.8/g' Makefile && make && sudo make install && cd mammut/external/libusb-1.0.9/ && sudo make install
